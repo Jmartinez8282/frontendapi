90e1348f33d6d94dad6c02101b92baacf78c13e3
jmartinez8282 <josemartinez8282@gmail.com>
2020-03-30 15:56:19 -0700

    cp

--- frontendstreaming/src/app/home/home.component.ts
+++ frontendstreaming/src/app/home/home.component.ts
@@ -13,6 +13,13 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     this.dService.GetTracks().subscribe(x=> console.log(x));
     this.dService.GetRandomTracks().subscribe(x=> console.log(x));
+    this.dService.addTrack({
+      artistName:"Norrin Radd",
+      trackName:"Go Home and Bea Family Man",
+      albumName:"Street Fighter 2",
+      trackUrl:"https://codestackcdnwest.blob.core.windows.net/muiscstreaming/Street_Fighter_2_GoHomeandBeaFamilyMan_OC_ReMix.mp3"
+
+    }).subscribe(x=> console.log(x));
   } 
 
 }
--- frontendstreaming/src/app/home/services/data.service.ts
+++ frontendstreaming/src/app/home/services/data.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-GetAllTracksURL= 'http://localhost:5000/streaming';
-RandomTrackURL ='http://localhost:5000/streaming/random';
+  public apiUrl = environment.api;
+GetAllTracksURL= this.apiUrl + 'streaming';
+RandomTrackURL =this.apiUrl + 'streaming/random';
+AddTrackURL = this.apiUrl + 'streaming';
   constructor(private http:HttpClient) { }
   GetTracks(){
     return this.http.get(this.GetAllTracksURL);
   }
   GetRandomTracks(){
     return this.http.get(this.RandomTrackURL)
+  }
+  addTrack(track:any){
+    return this.http.post(this.AddTrackURL,track)
   }
 }
